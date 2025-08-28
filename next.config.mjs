bd80b41a19a5b8b008b61e95c222a0e08a1decd0
Karan Roushan <karan.122roushan@gmail.com>
2025-08-28 13:48:57 +0530

    nothing updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['res.cloudinary.com', 'www.w3schools.com'], // add more if needed
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["res.cloudinary.com", "www.w3schools.com"], // add more if needed
+  },
+};
+
+export default nextConfig;
