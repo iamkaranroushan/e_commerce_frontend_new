0c10e13b25e81ffa23e25c9fd82f94bd04739941
Karan Roushan <karan.122roushan@gmail.com>
2025-11-21 14:23:42 +0530

    updated category id

--- components/custom/expandedModal.jsx
+++ components/custom/expandedModal.jsx
@@ -5,7 +5,7 @@ import ProductCarousel from "@/components/custom/productCarousal";
 import useProductsByCategory from "@/hooks/useProductsByCategory";
 
 const ExpandingModal = ({ onClose }) => {
-  const categoryId = "111";
+  const categoryId = "1";
   const { products, loading, error } = useProductsByCategory({ categoryId });
 
   return (
