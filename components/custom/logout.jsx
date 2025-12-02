1eedbae30bec48ce224a02242a06a6db5627fa3e
Karan Roushan <karan.122roushan@gmail.com>
2025-12-02 13:26:31 +0530

    changed logout modal UI

--- components/custom/logout.jsx
+++ components/custom/logout.jsx
@@ -24,29 +24,35 @@ const Logout = ({ onClose, setIsLogoutOpen }) => {
     };
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50 h-[100vh]">
-            <div className="flex flex-col p-5 gap-4 bg-white rounded-xl shadow-lg w-64 text-center">
-                <div className="flex justify-between items-center">
-                    <h2 className="text-md font-medium text-stone-800">Confirm logging out</h2>
+            <div className="flex flex-col p-5 gap-4 bg-white rounded-xl shadow-lg w-[clamp(15rem,30vw,18rem)]">
+                {/*heading and cross*/}
+                <div className="flex justify-between items-center w-full">
+                    <h2 className="text-md font-bold text-stone-800">Logging out</h2>
                     <span
                         onClick={onClose}
-                        className="text-sm text-stone-600 hover:underline"
+                        className="text-sm text-stone-800 rounded-full bg-stone-200  hover:text-stone-600 cursor-pointer transition-all p-1 "
                     >
-                        <RxCross1 className="text-md" />
+                        <RxCross1 size={14} className="text-md" />
                     </span>
                 </div>
-                <div className="flex justify-end items-center gap-4 pt-2">
-                    <Button
-                        variant="logout"
-                        onClick={handleLogout} // Replace with actual logout logic
-                    >
-                        Ok
-                    </Button>
+
+                 <h2 className="text-[clamp(0.9rem,3vw,1rem)]  text-stone-800">you will be logged out of this device</h2>
+
+                {/*ok and cancel button*/}
+                <div className="flex justify-end items-center space-x-2 pt-2">
+                   
                     <Button
                         disabled={loading}
                         variant="cancel"
                         onClick={onClose}
                     >
                         Cancel
+                    </Button>
+                     <Button
+                        variant="logout"
+                        onClick={handleLogout} // Replace with actual logout logic
+                    >
+                        Ok
                     </Button>
                 </div>
             </div>
