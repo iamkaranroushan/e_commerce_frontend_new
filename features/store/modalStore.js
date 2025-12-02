a125b809615650c0a9832536bb95fb463a4e406a
Karan Roushan <karan.122roushan@gmail.com>
2025-12-02 15:07:31 +0530

    added zustand store for modals

---
+++ features/store/modalStore.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+export const useModalStore = create((set) => ({
+  modal: null,        // null | "login" | "product" | "cart" etc.
+  modalProps: {},
+
+  openModal: (name, props = {}) =>
+    set({ modal: name, modalProps: props }),
+
+  closeModal: () =>
+    set({ modal: null, modalProps: {} }),
+}));
