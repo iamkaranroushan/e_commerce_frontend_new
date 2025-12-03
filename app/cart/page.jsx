073fff00eba9af52849c9c9862e0355ef59dd7ad
Karan Roushan <karan.122roushan@gmail.com>
2025-12-03 11:02:14 +0530

    set up the global modal portal

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,27 +1,29 @@
 "use client";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import Login from "@/components/custom/login";
 import CartWithoutLogin from "@/components/cart/cartWithoutLogin";
 import MainCart from "@/components/cart/mainCart";
+import { useModalStore } from "@/features/store/modalStore";
 
 const CartPage = () => {
-
   const token = useSelector((state) => state.auth.token);
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
 
+  const openModal = useModalStore((state) => state.openModal);
+  const closeModal = useModalStore((state) => state.closeModal);
 
-  return (<>
-        
-    <div className={`mt-16 ${isLoginOpen ? "blur-sm" : ""}`}>
+  return (
+    <div className="mt-16">
       {!token ? (
-        <CartWithoutLogin setIsLoginOpen={setIsLoginOpen} />
+        <CartWithoutLogin
+          openLoginModal={() => openModal("login")}
+        />
       ) : (
-        <MainCart setIsLoginOpen={setIsLoginOpen} token={token} />
-      )
-      }
+        <MainCart
+          openDeleteModal={(name, props) => openModal(name,props)}
+          token={token}
+          closeModal={closeModal}
+        />
+      )}
     </div>
-  </>
   );
 };
 
